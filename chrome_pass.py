729f896a5a42b6d1f953f3a23810b14f2eb41713
David White <david@digitalkiwi.co.nz>
2016-01-05 22:12:41 +1300

    modified:   chrome_pass.py

--- chrome_pass.py
+++ chrome_pass.py
@@ -1,14 +1,23 @@
-# ChromePass.py:	Decrypt passwords stored by Chrome in "login Data" file
+# ChromePass.py:	Decrypt passwords stored by Chrome in "login Data" file (Windows specific)
 # Author:    		David White
 # Date:      		October 2015
 
 from os import getenv
+import os
 import sqlite3
 import win32crypt
 
+path = getenv('APPDATA') + '\..\Local\Google\Chrome\User Data\Default\Login Data' #change as required
+if not os.path.isfile(path):
+    print '[!] Login Data file not found!\n'
+    quit()
+
+print '[!] Connecting to %s' % path
 # Connect to the database file
 try:
-    con = sqlite3.connect(getenv('APPDATA') + '\..\Local\Google\Chrome\User Data\Default\Login Data')
+    con = sqlite3.connect(path)
+    print '[!] Connected.  Decrypting passwords...\n'
+
 except Exception as e:
     print e
 
@@ -16,10 +25,12 @@ except Exception as e:
 curs = con.cursor()
 curs.execute('SELECT action_url, username_value, password_value FROM logins')
 for result in curs.fetchall(): # Decrypt the Password
-	site_name = result[0]
-	user_id = result[1]
-	password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
-	if password:
-		print 'Site: ' + site_name
-		print 'Username: ' + user_id
-		print 'Password: ' + password + '\n'
+    site_name = result[0]
+    user_id = result[1]
+    password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+    if password:
+        print 'Site: ' + site_name
+        print 'Username: ' + user_id
+        print 'Password: ' + password + '\n'
+
+print '[!] Done!\n'
